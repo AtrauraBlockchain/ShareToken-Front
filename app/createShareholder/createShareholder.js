5037cf3b3aaf1d75b4fedbe69aaeebd842324282
David <david@atraura.com>
2017-02-27 15:32:59 +0100

    ADD cryptoJS

--- app/createShareholder/createShareholder.js
+++ app/createShareholder/createShareholder.js
@@ -9,6 +9,22 @@ angular.module('myApp.createShareholder', ['ngRoute'])
   });
 }])
 
-.controller('CreateShareholderCtrl', [function() {
+.controller('CreateShareholderCtrl', ['$scope',function($scope) {
+  $scope.treatment = "Mr";
+  $scope.name = "";
+  $scope.surname = "";
+  $scope.id = "";
+  $scope.account = "";
+
+
+  $scope.generateHash = function() {
+      var CryptoJS = require("crypto-js");
+      var stringToEncrypt = "3/^5b,GhSh}%(,b*" +$scope.name + $scope.surname + $scope.id + $scope.account;
+      var ciphertext = CryptoJS.SHA3.encrypt('my message', { outputLength: 224 });
+
+      console.log(ciphertext.toString());
+
+  }
+
 
 }]);
